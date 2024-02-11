b3f1101074f5a2b26f19e7191b519b0b911fbfb2
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2024-02-11 09:49:28 +0100

    Make jsonschemastore its own package

---
+++ internal/jsonschemastore/store.go
@@ -0,0 +1,113 @@
+package jsonschemastore
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"path/filepath"
+
+	"github.com/bmatcuk/doublestar/v4"
+)
+
+type SchemaInfo struct {
+	Name      string   `json:"name"`
+	URL       string   `json:"url"`
+	FileMatch []string `json:"fileMatch"`
+}
+
+type JsonSchemaStore struct {
+	Index []SchemaInfo
+}
+
+func NewJsonSchemaStore() (JsonSchemaStore, error) {
+	indexResponse, err := callTheInternet("https://www.schemastore.org/api/json/catalog.json")
+	if err != nil {
+		return JsonSchemaStore{}, fmt.Errorf("Failed to download index: %s", err)
+	}
+	index, err := parseIndexResponse(indexResponse)
+	if err != nil {
+		return JsonSchemaStore{}, fmt.Errorf("Failed to parse index: %s", err)
+	}
+	return JsonSchemaStore{
+		Index: index,
+	}, nil
+}
+
+func parseIndexResponse(data []byte) ([]SchemaInfo, error) {
+	var indexResponse struct {
+		Schemas []SchemaInfo `json:"schemas"`
+	}
+	if err := json.Unmarshal(data, &indexResponse); err != nil {
+		return []SchemaInfo{}, fmt.Errorf("Failed to unmarshal index response: %s", err)
+	}
+	return indexResponse.Schemas, nil
+}
+
+var ErrorNoMatchingSchema = errors.New("No matching schema found")
+
+func (s *JsonSchemaStore) GetSchema(filename string) ([]byte, error) {
+	schemaInfo, found := getMatchingSchemaInfo(s.Index, filename)
+	if !found {
+		return nil, ErrorNoMatchingSchema
+	}
+	data, err := callTheInternet(schemaInfo.URL)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to call the internet: %s", err)
+	}
+	return data, nil
+}
+
+func (s *JsonSchemaStore) GetSchemaURL(filename string) (string, error) {
+	schemaInfo, found := getMatchingSchemaInfo(s.Index, filename)
+	if !found {
+		return "", ErrorNoMatchingSchema
+	}
+	return schemaInfo.URL, nil
+}
+
+func getMatchingSchemaInfo(index []SchemaInfo, filename string) (SchemaInfo, bool) {
+	for _, schemaInfo := range index {
+		for _, pattern := range schemaInfo.FileMatch {
+			if matchFilePattern(pattern, filename) {
+				return schemaInfo, true
+			}
+		}
+	}
+	return SchemaInfo{}, false
+}
+
+func matchFilePattern(pattern string, filename string) bool {
+	match := false
+	if filepath.Base(pattern) == pattern {
+		// The pattern only matches on the basename
+		filename := filepath.Base(filename)
+		m, err := filepath.Match(pattern, filename)
+		if err == nil {
+			match = m
+		}
+	} else {
+		m, err := doublestar.Match(pattern, filename)
+		if err == nil {
+			match = m
+		}
+	}
+	return match
+}
+
+func callTheInternet(URL string) ([]byte, error) {
+	resp, err := http.Get(URL)
+	if err != nil {
+		return []byte{}, err
+	}
+	if resp.StatusCode != 200 {
+		return []byte{}, fmt.Errorf("Got non-200 status code: %s", resp.Status)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
+}
