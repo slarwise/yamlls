bc75c7c3a3add35d70ab06ccbb26b684e50e05bf
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2024-02-11 21:33:29 +0100

    Add crdstore to schemastore

--- internal/schemas/schemas.go
+++ internal/schemas/schemas.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/url"
 
+	"github.com/slarwise/yamlls/internal/crdstore"
 	. "github.com/slarwise/yamlls/internal/errors"
 	"github.com/slarwise/yamlls/internal/jsonschemastore"
 	"github.com/slarwise/yamlls/internal/kubernetesstore"
@@ -12,6 +13,7 @@ import (
 
 type SchemaStore struct {
 	kubernetesStore kubernetesstore.KubernetesStore
+	crdStore        crdstore.CRDStore
 	jsonSchemaStore jsonschemastore.JsonSchemaStore
 }
 
@@ -20,12 +22,17 @@ func NewSchemaStore() (SchemaStore, error) {
 	if err != nil {
 		return SchemaStore{}, fmt.Errorf("Could not create kubernetes schema store: %s", err)
 	}
+	crdStore, err := crdstore.NewCRDStore()
+	if err != nil {
+		return SchemaStore{}, fmt.Errorf("Could not create CRD schema store: %s", err)
+	}
 	jsonSchemaStore, err := jsonschemastore.NewJsonSchemaStore()
 	if err != nil {
 		return SchemaStore{}, fmt.Errorf("Could not create json schema store: %s", err)
 	}
 	return SchemaStore{
 		kubernetesStore: kubernetesStore,
+		crdStore:        crdStore,
 		jsonSchemaStore: jsonSchemaStore,
 	}, nil
 }
@@ -34,6 +41,13 @@ func (s *SchemaStore) GetSchema(filename string, text string) ([]byte, error) {
 	group, version, kind := parser.GetGroupVersionKind(text)
 	if version != "" && kind != "" {
 		schema, err := s.kubernetesStore.GetSchema(group, version, kind)
+		if err == nil {
+			return schema, nil
+		}
+		if err != ErrorSchemaNotFound {
+			return []byte{}, fmt.Errorf("Error when fetching schema: %s", err)
+		}
+		schema, err = s.crdStore.GetSchema(group, version, kind)
 		if err == nil {
 			return schema, nil
 		}
@@ -56,6 +70,10 @@ func (s *SchemaStore) GetSchemaURL(filename string, text string) (string, error)
 	group, version, kind := parser.GetGroupVersionKind(text)
 	if group != "" && version != "" && kind != "" {
 		URL, err := s.kubernetesStore.GetSchemaURL(group, version, kind)
+		if err == nil {
+			return URL, nil
+		}
+		URL, err = s.crdStore.GetSchemaURL(group, version, kind)
 		if err == nil {
 			return URL, nil
 		}
